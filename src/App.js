a11613ddbe56071fb8936f88d2eb5ac90ffbfc58
GautamR24 <rawatgautam24@protonmail.com>
2023-02-17 12:37:39 +0530

    worked on dark mode

--- src/App.js
+++ src/App.js
@@ -3,14 +3,26 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import About from './components/About';
+import { useState } from 'react';
 
 function App() {
+  const [mode,setMode] = useState('light');
+
+  const toggle = () =>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#051e42';
+    }else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
     <>
-      <Navbar title="TextUtil"/>
+      <Navbar title="TextUtil" mode={mode} toggle={toggle}/>
       <div className="container my-3">
-          <About/>
-          <TextForm heading="Enter the text to analyze below"/>
+          {/* <About/> */}
+          <TextForm heading="Enter the text to analyze below" mode={mode}/>
           
       </div>
     </>
