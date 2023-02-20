beeb8039650ada3c64e7d30117bdade52bedef9e
GautamR24 <rawatgautam24@protonmail.com>
2023-02-20 13:24:42 +0530

    router added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-//import About from './components/About';
+import About from './components/About';
 import { useState } from 'react';
 import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   const [mode,setMode] = useState('light');
@@ -32,13 +38,17 @@ function App() {
   }
   return (
     <>
+    <Router>
       <Navbar title="TextUtil" mode={mode} toggle={toggle}/>
       <Alert alert={alert}/>
       <div className="container my-3">
-          {/* <About/> */}
-          <TextForm heading="Enter the text to analyze below" mode={mode} showAlert={showAlert}/>
+      <Routes>
+          <Route exact path="/about" element={<About/>}/>
+          <Route exact path="/" element={<TextForm heading="Enter the text to analyze below" mode={mode} showAlert={showAlert}/>}/>
+      </Routes>
           
       </div>
+      </Router>
     </>
   );
 }
